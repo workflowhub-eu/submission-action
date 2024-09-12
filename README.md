6b224656a9ec795237a32801e8735f80d4dcd828
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-09-12 13:30:15 +0100

    Fix outputs, format log nicely, more examples

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
-# submission-action
-GitHub action to submit a workflow repository to WorkflowHub
+# WorkflowHub Submission Action
+A GitHub action to submit a workflow repository to WorkflowHub.
 
-## Example usage:
+## Prerequisites
+
+1. [Create a WorkflowHub account](https://about.workflowhub.eu/docs/how-to-register/)
+2. [Create or join an existing WorkflowHub team](https://about.workflowhub.eu/docs/join-create-teams-spaces/)
+3. Create a WorkflowHub API token (Menu > My Profile > Actions > API Tokens > New API Token)
+4. Add your API Token as a secret in GitHub https://docs.github.com/en/actions/security-for-github-actions/security-guides/using-secrets-in-github-actions (named either `WORKFLOWHUB_API_TOKEN` or `DEV_WORKFLOWHUB_API_TOKEN` depending on which instance you are submitting to)
+
+## Examples
+
+#### Submit workflow to WorkflowHub when a new release is published on GitHub
+
 ```yaml
 name: Publish workflows on WorkflowHub
 
-on: push
+on:
+  release:
+    types: [published]
 
 jobs:
   wfh-submit:
-    name: WorkflowHub Submission
+    name: WorkflowHub submission
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v4
@@ -21,3 +33,28 @@ jobs:
           team_id: 123
 ```
 
+#### Test workflow submission using dev.workflowhub.eu
+
+This requires an account, token and team to be created on the WorkflowHub dev/sandbox instance: https://dev.workflowhub.eu
+
+This action is triggered manually: https://docs.github.com/en/actions/managing-workflow-runs-and-deployments/managing-workflow-runs/manually-running-a-workflow
+
+```yaml
+name: Test workflow publishing on dev.WorkflowHub
+
+on: workflow_dispatch
+
+jobs:
+  wfh-submit:
+    name: WorkflowHub submission test
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - name: Submit workflows
+        uses: workflowhub-eu/submission-action@v0
+        env:
+          API_TOKEN: ${{ secrets.DEV_WORKFLOWHUB_API_TOKEN }}
+        with:
+          team_id: 123
+          instance: https://dev.workflowhub.eu
+```
--- submit.rb
+++ submit.rb
@@ -7,65 +7,66 @@ raise "Missing API_TOKEN" if ENV['API_TOKEN'].nil?
 OUTPUT = File.open(ENV['GITHUB_OUTPUT'], 'w')
 puts "Switching to: #{ENV['GITHUB_WORKSPACE']}"
 Dir.chdir(ENV['GITHUB_WORKSPACE'])
+puts "::error title=test::error test"
 
-begin
-  config = {}
-  if File.exist?('.workflowhub.yml')
-    config = YAML.load_file('.workflowhub.yml')
-    puts config.inspect
-  end
+config = {}
+config = YAML.load_file('.workflowhub.yml') if File.exist?('.workflowhub.yml')
 
-  puts "Arguments: #{ARGV.inspect}"
+team_id = config.delete('team_id') || ARGV[0]
+raise "Missing Team ID" if team_id.nil?
+puts "::debug::Team ID: #{team_id}"
 
-  team_id = config.delete('team_id') || ARGV[0]
-  puts "Team ID: #{team_id}"
-  raise "Missing Team ID" if team_id.nil?
-  instance = config.delete('instance') || ARGV[1] || 'https://workflowhub.eu'
-  puts "Instance: #{instance}"
+instance = config.delete('instance') || ARGV[1] || 'https://workflowhub.eu'
+puts "::debug::Instance: #{instance}"
 
-  url = "#{instance.chomp('/')}/workflows/submit"
-  puts config.inspect
+url = "#{instance.chomp('/')}/workflows/submit"
+workflows = config['workflows'] || [config]
+workflow_urls = []
+workflows.each_with_index do |workflow, i|
+  path = workflow.delete('path') || '.'
+  metadata_path = Pathname.new(path).join('ro-crate-metadata.json')
+  if metadata_path.exist?
+    puts "Found: #{path}"
+    crate = ROCrate::Reader.read(path)
+    file = "crate-#{i}.crate.zip"
+    puts "  Zipping..."
+    ROCrate::Writer.new(crate).write_zip(file)
 
-  workflows = config['workflows'] || [config]
-  puts workflows
-  workflows.each_with_index do |workflow, i|
-    path = workflow.delete('path') || '.'
-    metadata_path = Pathname.new(path).join('ro-crate-metadata.json')
-    if metadata_path.exist?
-      puts "Found: #{path}"
-      crate = ROCrate::Reader.read(path)
-      file = "crate-#{i}.crate.zip"
-      puts "  Zipping..."
-      ROCrate::Writer.new(crate).write_zip(file)
+    body = {
+      'workflow[project_ids][]' => team_id,
+      'ro_crate' => File.open(file, 'rb'),
+      multipart: true
+    }
 
-      body = { 'workflow[project_ids][]' => team_id,
-               'ro_crate' => File.open(file, 'rb'),
-               multipart: true }
+    headers = {
+      'Authorization' => "Token #{ENV['API_TOKEN']}",
+      'User-Agent' => 'GH (https://github.com/workflowhub-eu/submission-action)'
+    }
 
-      headers = {
-        'Authorization' => "Token #{ENV['API_TOKEN']}",
-        'User-Agent' => 'GH (https://github.com/workflowhub-eu/submission-action)'
-      }
-
-      puts "  Uploading..."
-      begin
-        response = JSON.parse(RestClient.post(url, body, **headers))
-      rescue RestClient::ExceptionWithResponse => e
-        STDERR.puts "Error uploading RO-Crate: #{e.message}"
-        STDERR.puts e.http_body
-        exit 1
-      else
-        location = "#{instance.chomp('/')}#{response.dig('data', 'links', 'self')}"
-        puts "  Done: #{location}"
-        OUTPUT.puts(location)
-      end
+    puts "  Uploading..."
+    begin
+      response = JSON.parse(RestClient.post(url, body, **headers))
+    rescue RestClient::ExceptionWithResponse => e
+      STDERR.puts "Error uploading RO-Crate: #{e.message}"
+      STDERR.puts e.http_body
+      exit 1
     else
-      raise "Couldn't find #{metadata_path}"
+      location = "#{instance.chomp('/')}#{response.dig('data', 'links', 'self')}"
+      puts "  Done: #{location}"
     end
+  else
+    raise "Couldn't find #{metadata_path}"
   end
-ensure
-  OUTPUT.close
+  puts
+
+  File.open(ENV['GITHUB_OUTPUT'], 'w') do |out|
+    out.puts("workflow_urls<<EOF")
+    workflow_urls.each do |u|
+      out.puts(u)
+    end
+    out.puts("EOF")
+  end
 end
 
-puts "\nFinished!"
-exit 0
+File.write(ENV['GITHUB_STEP_SUMMARY'], "#{workflow_urls.count} workflows submitted")
+puts "Finished!"
